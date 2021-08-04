af2f138215c7b1042cd309a757af825c27c1fe60
Sayooj <dev.sayooj@gmail.com>
2021-08-04 12:11:04 +0530

    Initial Commit

---
+++ BankingTrainingVIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingTrainingVIPER
+//
+//  Created by Sayooj Krishnan  on 04/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
